37aa36b0fd0403e14467024549f54969021609c8
SamuelCastrillon <samcastj@gmail.com>
2024-07-18 22:28:08 -0500

    Add Shearch Bar to Navbar

--- front/src/components/LogoApp/LogoApp.tsx
+++ front/src/components/LogoApp/LogoApp.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const LogoApp: React.FC = () => {
-  return <div>LogoApp</div>;
+  return (
+    <Link href="/home">
+      <div className="flex items-center ml-1">
+        <div className="w-10 h-10 bg-lime-200 rounded-e-full"></div>
+        <strong>igital Innovation</strong>
+      </div>
+    </Link>
+  );
 };
 
 export default LogoApp;
