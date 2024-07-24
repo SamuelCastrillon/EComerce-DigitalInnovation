508b2e43e3e4566099116686703e0cf42653d9af
SamuelCastrillon <samcastj@gmail.com>
2024-07-24 09:49:39 -0500

    fix 404 not-found

--- front/src/app/not-found.tsx
+++ front/src/app/not-found.tsx
@@ -1,7 +1,19 @@
-import NotFoundView from "@/views/NotFound/NotFoundView";
+import Image404 from "@/assets/img/404NotFound.jpg";
+import Link from "next/link";
 
 const NotFound: React.FC = () => {
-  return <NotFoundView />;
+  return (
+    <section className="relative flex flex-col gap-5 mt-20">
+      <div className="">
+        <img src={Image404.src} alt="404 Notfund Image" className="h-auto w-max  rounded-[50%]" />
+      </div>
+      <button className="px-5 py-2 rounded bg-lime-950">
+        <Link href="/home" className="font-bold">
+          Return Home
+        </Link>
+      </button>
+    </section>
+  );
 };
 
 export default NotFound;
